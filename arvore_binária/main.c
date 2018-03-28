261bbdded8e4aa4fdda82a1beb38eac37e29ab97
Victor Barbosa Rocha <victor.barbosa.18@outlook.com>
2018-03-27 21:41:16 -0300

    QUESTÃO 1

---
+++ arvore_binária/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+
+int main()
+{
+
+    Arv* oak = arv_cria(arv_cria_no('a',
+                                     arv_cria_no('b',
+                                                  NULL, arv_cria_no('d',NULL, NULL)
+                                                  ),
+                                     arv_cria_no('c',
+                                                 arv_cria_no('e', NULL, NULL),
+                                                 arv_cria_no('f', NULL, NULL)
+                                                 )
+                                     )
+                        );
+
+    imprime_arv(oak, 1);
+
+
+    return 0;
+}
